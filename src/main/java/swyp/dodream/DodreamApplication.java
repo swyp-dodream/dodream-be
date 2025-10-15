99eaa7df3cf8479ed93b3344db358c7e5fe35dae
hoonjo123 <johoon030@gmail.com>
2025-10-15 21:43:58 +0900

    feature/초기세팅

---
+++ src/main/java/swyp/dodream/DodreamApplication.java
@@ -0,0 +1,13 @@
+package swyp.dodream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodreamApplication.class, args);
+    }
+
+}
