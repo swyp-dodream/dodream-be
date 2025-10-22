4136a431fa20ea42118a1574002c28fa924175d3
johoon030 <142702766+hoonjo123@users.noreply.github.com>
2025-10-22 23:14:05 +0900

    feat: swagger 수정 (#15)

--- src/main/java/swyp/dodream/config/SwaggerConfig.java
+++ src/main/java/swyp/dodream/config/SwaggerConfig.java
@@ -2,11 +2,8 @@ package swyp.dodream.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,14 +11,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("DoDream API")
-                .description("DoDream Backend API")
-                .version("v0.1.0");
+                .info(new Info()
+                        .title("DoDream API")
+                        .description("DoDream 백엔드 API 문서")
+                        .version("v1.0"));
     }
 }
-
