17982b0b2adb9dc83b0d24a54a726b49fa719d3f
sohee52 <wneh2023@naver.com>
2025-10-29 23:34:25 +0900

    feat: 북마크 추가/해제/조회 기능 추가

---
+++ src/main/java/swyp/dodream/domain/post/repository/BookmarkRepository.java
@@ -0,0 +1,13 @@
+package swyp.dodream.domain.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import swyp.dodream.domain.post.domain.Bookmark;
+import swyp.dodream.domain.post.domain.Post;
+import swyp.dodream.domain.user.domain.User;
+import java.util.List;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    boolean existsByUserAndPost(User user, Post post);
+    void deleteByUserAndPost(User user, Post post);
+    List<Bookmark> findAllByUser(User user);
+}
