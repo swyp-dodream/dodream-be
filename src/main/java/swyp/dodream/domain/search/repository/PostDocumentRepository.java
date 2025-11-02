b2771ffc82948d030512e0eca226c68b4bed6ad8
sohee52 <wneh2023@naver.com>
2025-11-02 19:01:53 +0900

    feat(search): ES 기반 게시글 검색 API 구현 및 Document 인덱싱 연동

--- src/main/java/swyp/dodream/domain/search/repository/PostDocumentRepository.java
+++ src/main/java/swyp/dodream/domain/search/repository/PostDocumentRepository.java
@@ -1,8 +1,24 @@
 package swyp.dodream.domain.search.repository;
 
+import org.springframework.data.elasticsearch.annotations.Query;
 import swyp.dodream.domain.search.document.PostDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface PostDocumentRepository extends ElasticsearchRepository<PostDocument, Long> {
+
+    @Query("""
+            {
+              "bool": {
+                "should": [
+                  { "match": { "title": "?0" } },
+                  { "match": { "content": "?0" } }
+                ]
+              }
+            }
+            """)
+    List<PostDocument> searchByTitleOrContent(String keyword);
+
 }
 
