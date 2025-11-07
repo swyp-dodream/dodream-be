94a17c17eb9f5743e1367d05428865d79cf8927f
johoon030 <142702766+hoonjo123@users.noreply.github.com>
2025-11-07 20:08:00 +0900

    oauth테이블 사용하도록 수정 (#39)

--- src/main/java/swyp/dodream/domain/user/domain/User.java
+++ src/main/java/swyp/dodream/domain/user/domain/User.java
@@ -23,17 +23,7 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
-    private String email;
-
     private String profileImageUrl;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private swyp.dodream.login.domain.AuthProvider provider;
-
-    @Column(nullable = false)
-    private String providerId;
 
     @Column(nullable = false)
     private Boolean status = true;
@@ -45,22 +35,16 @@ public class User {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    public User(String name, String email, String profileImageUrl, swyp.dodream.login.domain.AuthProvider provider, String providerId) {
+    public User(String name, String profileImageUrl) {
         this.name = name;
-        this.email = email;
         this.profileImageUrl = profileImageUrl;
-        this.provider = provider;
-        this.providerId = providerId;
     }
     
     // Snowflake ID를 사용하는 생성자
-    public User(Long id, String name, String email, String profileImageUrl, swyp.dodream.login.domain.AuthProvider provider, String providerId) {
+    public User(Long id, String name, String profileImageUrl) {
         this.id = id;
         this.name = name;
-        this.email = email;
         this.profileImageUrl = profileImageUrl;
-        this.provider = provider;
-        this.providerId = providerId;
     }
 
     // Update methods
